70fd7f7a98fdf0a8c37479df9bf93039ef966301
Miguel España <89494418+EspanaLokote13@users.noreply.github.com>
2021-12-03 00:53:38 -0800

    Add files via upload

---
+++ JuegoDeMemoria/src/Principal.java
@@ -0,0 +1,15 @@
+/*
+Clase: Principal
+Responsabilidad: Inicia el programa y crea un objeto Vista de GuiMemoria
+Colaboracion: Ninguna
+*/
+import javax.swing.JFrame;
+
+public class Principal 
+{
+	public static void main(String[] args) 
+	{
+		MemoriaGUI vista = new MemoriaGUI();
+		vista.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
